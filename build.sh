bb15a48b511269af735002a4630ff96f1f9ea55f
mdalexca <matthewalex@linux.com>
2017-01-03 20:29:47 -0500

    build: R19

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-6.x/bin/aarch6
 DEFCONFIG="sharkey_defconfig"
 
 # Kernel Details
-VER=".R18.marlin"
+VER=".R19.marlin"
 
 # Paths
 KERNEL_DIR=`pwd`
