b4dc158b1c29a55d2413b15495ff662d25f2b25c
mdalexca <matthewalex@linux.com>
2017-01-08 15:02:25 -0500

    build: R21

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-6.x/bin
 DEFCONFIG="sharkey_defconfig"
 
 # Kernel Details
-VER=".R20.marlin"
+VER=".R21.marlin"
 
 # Paths
 KERNEL_DIR=`pwd`
