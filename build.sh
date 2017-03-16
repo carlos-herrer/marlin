10e20b71a753494b82dc06d269e073cd4a5c05dd
DespairFactor <matthewalex@linux.com>
2017-03-16 17:12:52 -0400

    build: R45

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-6.x/bin
 DEFCONFIG="sharkey_defconfig"
 
 # Kernel Details
-VER=".R44.marlin"
+VER=".R45.marlin"
 
 # Paths
 KERNEL_DIR=`pwd`
